e7376d4c0331f6848b4e8d1991cd5a567efab77a
Hubert Dryja <hubertdryja@gmail.com>
2019-03-04 12:22:10 +0100

    init

---
+++ moviesapi/settings/production.py
@@ -0,0 +1,19 @@
+import os
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+from .base import *
+
+sentry_sdk.init(
+    dsn=os.environ['SENTRY_KEY'], integrations=[DjangoIntegration()])
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db_name',
+        'USER': 'db_user',
+        'PASSWORD': 'db_user_password',
+        'HOST': '',
+        'PORT': 'db_port_number',
+    }
+}
